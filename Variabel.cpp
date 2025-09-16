40376f586963e29a94ece9180a68bfa13bb876d2
tristan25-el <tristanbelajar25@gmail.com>
2025-09-17 01:06:26 +0700

    Create Variabel.cpp

---
+++ Variabel.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main (){
+	int angka = 10 ;
+	float desimal =5.5 ;
+	char karakter = 'A';
+
+	int a=1,b=2,c=3;
+
+	cout<< "Nilai angka: "<<angka<<endl;
+	cout<< "Nilai desimal: "<<desimal<<endl;
+	cout<< "Nilai karakter: "<<karakter<<endl;
+	cout<< "Nilai a: "<<a<<endl;
+	cout<< "Nilai b: "<<b<<endl;
+	cout<< "Nilai c: "<<c<<endl;
+	return 0;
+}
+
