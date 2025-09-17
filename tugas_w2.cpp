d1e8ea629cc12b854a019feb4f932239ff5374aa
tristan25-el <tristanbelajar25@gmail.com>
2025-09-17 16:10:51 +0700

    Add files via upload

---
+++ tugas_w2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a,b,c,d,e,f,g,h,i;
+
+    cout<<"Masukan data matriks baris 1: ";
+    cin>>a>>b>>c;
+    cout<<"Masukan data matriks baris 2: ";
+    cin>>d>>e>>f;
+    cout<<"Masukan data matriks baris 3: ";
+    cin>>g>>h>>i;
+
+    cout<<"Data yang anda masukkan : "<<endl;
+
+    cout<<a<<" "<<b<<" "<<c<<endl;
+    cout<<d<<" "<<e<<" "<<f<<endl;
+    cout<<g<<" "<<h<<" "<<i<<endl;
+
+	return 0;
+}
+
