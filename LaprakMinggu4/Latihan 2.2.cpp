933f157b033421e8766b7e606e5caec95a17ffad
tristan25-el <tristanbelajar25@gmail.com>
2025-09-30 14:16:28 +0700

    Add files via upload

---
+++ LaprakMinggu4/Latihan 2.2.cpp
@@ -0,0 +1,18 @@
+#include<iostream> 
+#include<fstream> 
+using namespace std; 
+int main(){ 
+	int data,n;
+	 
+	ifstream file("data_inputR.txt");  
+		cout<<"File sukses dibuka!"<<endl; 
+ 
+	for(int i=0; i<7 ; i++){ 
+		file>>data; 
+		cout<<data<<endl; 
+	} 
+ 
+	file.close(); 
+	return 0; 
+	
+} 
